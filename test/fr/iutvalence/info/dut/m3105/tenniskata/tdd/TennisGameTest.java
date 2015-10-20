75ca1e06b45ea3bdc432587d0cd18873731a7a69
bertholm <bertholm@PC-B101-04.iut-valence.fr>
2015-10-20 13:06:28 +0200

    Iteration 2: red

--- test/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGameTest.java
+++ test/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGameTest.java
@@ -1,17 +1,20 @@
 package fr.iutvalence.info.dut.m3105.tenniskata.tdd;
 
-
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class TennisGameTest
-{
+public class TennisGameTest {
 	@Test
-	public void ifThereIsALoveAllSituation()
-	{
+	public void atTheBeginningOfTheGameTheScoreShouldBeLoveAll() {
 		TennisGame tennisGame = new TennisGame();
 		assertEquals(tennisGame.getScore(), "love-all");
 	}
 
+	@Test
+	public void ifTheServerMakesTheFirstPoint() {
+		TennisGame tennisGame = new TennisGame();
+		assertEquals(tennisGame.getScore(), "15-0");
+	}
+	
 }
